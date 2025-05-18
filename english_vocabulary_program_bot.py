9ba00327b2ac991fb680b7fc3513571b49fa3df7
Kirill <kirill24wr@gmail.co>
2025-05-18 21:26:35 +0500

    Обновление функционала

--- english_vocabulary_program_bot.py
+++ english_vocabulary_program_bot.py
@@ -2,37 +2,42 @@ import random
 
 class Words:
     def __init__(self):
-        self.__array = ["a", "b", "c", "d", "f"]
+        self.__words = ["a", "b", "c", "d", "f"]
 
-    def user_input(self):
-        user_input_repeat = int(input("How many words do you want to enter: "))
+
+    def add_words(self, user_input_repeat, user_input_words):
         for _ in range(user_input_repeat):
-            user_input_words = input("Enter the word: ")
-            self.__array.append(user_input_words)
+            self.__words.append(user_input_words)
+
 
     def get_random_elements(self, k):
-        n = len(self.__array)
-        arr_copy = self.__array[:]
+        n = len(self.__words)
+        arr_copy = self.__words[:]
         result = []
 
         for i in range(k):
             index = int(random.random() * (n - i))
             result.append(arr_copy[index])
             arr_copy[index], arr_copy[n - i - 1] = arr_copy[n - i - 1], arr_copy[index]
-        print("Random words:", result)
+
+        return result
 
 
 def repeat_func(w, k):
     repeat = input("Do you want to enter another word (y), view 5 words (v), or end game (n)? ")
     if repeat == "y":
-        w.user_input()
+        user_input_repeat = int(input("How many words do you want to enter: "))
+        for _ in range(user_input_repeat):
+            user_input_words = input("Enter the word: ")
+            w.add_words(1, user_input_words)
     elif repeat == "v":
-        w.get_random_elements(k)
+        result = w.get_random_elements(k)
+        print("Random words:", result)
     elif repeat == "n":
         print("Come back again!")
         return False
     else:
-        print("Please enter 'y', 'v' or 'n'.")
+        print("Invalid input. Please enter 'y', 'v', or 'n'.")
     return True
 
 
@@ -40,7 +45,11 @@ def main():
     print("Welcome to the English vocabulary program!")
     k = 5
     w = Words()
-    w.user_input()
+    user_input_repeat = int(input("How many words do you want to enter: "))
+    for _ in range(user_input_repeat):
+        user_input_words = input("Enter the word: ")
+        w.add_words(1, user_input_words)
+
 
     repeat = True
     while repeat:
