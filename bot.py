c61f3e1ada6d54365799e4357f6fbe19a58a15ed
Kirill <kirill24wr@gmail.co>
2025-07-30 14:23:02 +0500

    #

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import random
 a = [1, 2, 3, 4, 5]
 
 
-
 for i in range(5):
     b = round(random.random() * 4)
     print(a[b])
--- calculator.py
+++ calculator.py
@@ -1,3 +1,19 @@
 num1 = int(input("Первое число: "))
 num2 = int(input("Второе число: "))
 c = (input("Выберите одну из операций: + - / * "))
+
+if c == "+":
+    result = num1 + num2
+    print(result)
+
+elif c == "-":
+    result = num1 - num2
+    print(result)
+
+elif c == "/":
+    result = num1 / num2
+    print(result)
+
+elif c == "*":
+    result = num1 * num2
+    print(result)
